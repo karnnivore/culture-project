84ccb4cb16253dea9536c2e71a4ab0cc6d1bc8dd
Nick Chinsen <nicholaschinsen@gmail.com>
2020-12-26 10:06:32 -0500

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+require("dotenv").config()
+
 module.exports = {
   siteMetadata: {
     title: "Culture Project",
@@ -14,6 +16,14 @@ module.exports = {
         path: "./src/images/",
       },
       __key: "images",
+    },
+    {
+      resolve: "gatsby-plugin-web-font-loader",
+      options: {
+        typekit: {
+          id: process.env.TYPEKIT_ID,
+        },
+      },
     },
   ],
 };
