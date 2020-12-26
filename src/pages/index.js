de4a8bbf6b06cb59eedde5596db8fc7545fec1d8
Nick Chinsen <nicholaschinsen@gmail.com>
2020-12-26 11:59:39 -0500

    Updated nav bar

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,60 @@ import * as React from "react"
 // markup
 const IndexPage = () => {
   return (
-    <div id="main-wrapper">
-      <div class="section_head">
-        "Why I'm doing this"
+    <div>
+      <div class="outernav">
+        <div class="outernav-top">
+          <a href="https://nickchinsen.com" class="nick-chinsen">
+            <div class="outernav-img">
+              <img class="manual" src="/nickchinsen.svg" alt="manual"/>
+            </div>
+          </a>
+          <div class="outernav-tr">
+            <ul class="outernav-weather">
+              <li class="outernav-weather-item">
+                NYC 
+                <span class="temp">
+                  28
+                  °F
+                </span>
+              </li>
+              <li class="outernav-weather-item">
+                | London 
+                <span class="temp">
+                  10
+                  °C
+                </span>
+              </li>
+              <li class="outernav-weather-item">
+                | Toronto 
+                <span class="temp">
+                  -4
+                  °C
+                </span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="outernav-bottom">
+
+        </div>
       </div>
+    <div class="index">
+      <div class="index_content">
+        <div class="index_link">
+          Why I'm doing this
+        </div>
+        <div class="index_link">
+          Values I live by
+        </div>
+        <div class="index_link">
+          How I make things
+        </div>
+        <div class="index_link">
+          FAQ
+        </div>
+      </div>
+    </div>
     </div>
   )
 }
